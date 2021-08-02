6fe3ee0eac9bedddc5a74541f274ebc44588eab8
Tuğçegül Acar <66126964+tugcegula@users.noreply.github.com>
2021-08-03 01:57:55 +0300

    delete

--- HepsiEmlak2/src/main/java/com/hepsiemlak/constants/ConstantMain.java
+++
@@ -1,7 +0,0 @@
-package com.hepsiemlak.constants;
-
-import org.openqa.selenium.By;
-
-public class ConstantMain {
-    public static final By satilik = By.xpath("//*[@id=\"__layout\"]/div/section[1]/div[1]/div/div[2]/div/ul/li[3]/a");
-}
